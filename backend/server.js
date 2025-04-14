a9384a9ff5d3e2d5d7ea909e7e1047a854a8362a
Razan <razan.ghanem605@gmail.com>
2025-04-14 19:55:13 +0200

    server.js

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// connect to mongo
+connectDB();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/events", require("./routes/eventRoutes"));
+app.use("/api/bookings", require("./routes/bookingRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
