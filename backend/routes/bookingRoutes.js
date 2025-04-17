6c2d563643124b023b7f97bc91d83f91695ec074
GamilaHegazy <gamilakhaledhegazy@gmail.com>
2025-04-17 22:50:58 +0200

    This is the booking part (d) supposedly should be working (I tried T_T)

---
+++ backend/routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bookingController = require('../controllers/bookingController');
+const authMiddleware = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.post('/', authMiddleware, bookingController.createBooking);
+
+router.get('/:id', authMiddleware, bookingController.getBookingById); 
+
+router.delete('/:id', authMiddleware, bookingController.cancelBookings);
+
+router.get('/users/booking', authMiddleware, bookingController.getCurrentBookings);
