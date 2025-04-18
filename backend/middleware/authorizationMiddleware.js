5eb2fe15201bcb4b95fd482992ed113685300a32
Razan <razan.ghanem605@gmail.com>
2025-04-18 22:06:32 +0200

    user controller and routes + authentication and authorization middleware

---
+++ backend/middleware/authorizationMiddleware.js
@@ -0,0 +1,15 @@
+module.exports = function authorizationMiddleware(roles) {
+    return (req, res, next) => {
+      if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized: No user info" });
+      }
+  
+      const userRole = req.user.role;
+      if (!roles.includes(userRole)) {
+        return res.status(403).json({ message: "Forbidden: Access denied" });
+      }
+  
+      next();
+    };
+  };
+  
